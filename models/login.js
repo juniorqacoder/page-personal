d15b852c3cc857c9151b88ee2b34d97270fe4a7e
Junior | QA & Dev <59706105+juniorqualidev@users.noreply.github.com>
2025-01-03 21:09:40 +0000

    Inclusao de docker compose

---
+++ models/login.js
@@ -0,0 +1,9 @@
+function logar(user, password) {
+  if (password == 123) {
+    return "Logado com sucesso!";
+  } else {
+    return "Usuário ou paciente incorreto";
+  }
+}
+
+exports.logar = logar;
