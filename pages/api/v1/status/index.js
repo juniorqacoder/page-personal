99546d58d7d509929c2d06bdc596c823fbefabac
Junior | QA & Dev <59706105+juniorqualidev@users.noreply.github.com>
2025-01-06 16:15:35 +0000

    Ajustes de

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  return response.status(200).json({nome:"Teste"});
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("select 1 + 1 as SUM;");
+  console.log("Result: ", result.rows);
+  return response.status(200).json({return: result.rows});
 }
 
 export default status;
--- tests/integration/api/v1/get..test.js
+++ tests/integration/api/v1/get..test.js
@@ -1,4 +1,6 @@
 test('Get to /api/status should return 200', async ()=>{
   const response = await  fetch('http://localhost:3000/api/v1/status')
   expect(response.status).toBe(200)
+  console.log(response)
+  // expect(.json).toBe(200)
 })
