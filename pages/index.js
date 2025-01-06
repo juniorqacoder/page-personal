b774125aa03335ee29b8df590300ab0ab5898234
Junior | QA & Dev <59706105+juniorqualidev@users.noreply.github.com>
2025-01-06 21:39:09 +0000

    Adjust file

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ function Home() {
       <h1>JUNIOR DEV QA (Gilmario Junior)</h1>
       <span>Tech Lead QA - Desenvolvedor Sênior</span>
       <span>
+      <br />
         Trilha para realizar a sua migração de carreira ou iniciar com
         desenvolvimento||QA
       </span>
+      <br />
       <span>
         Teremos trilhas separadas para desenvolvedores e QAS para consuloria
         entre em contato com gilamrio@qaxsoltuions.com
