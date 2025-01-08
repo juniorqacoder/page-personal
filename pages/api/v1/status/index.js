f66addf2d06c2d86d8851a94c99088a60012a5e5
Junior | QA & Dev <59706105+juniorqualidev@users.noreply.github.com>
2025-01-08 21:48:13 +0000

    Adjust general end Foundation

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,18 +1,21 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
+  const dataName = process.env.POSTGRES_DB;
   const updateAt = new Date().toISOString();
   const maxConnections = await database.query("show max_connections");
   const version = await database.query("show server_version");
-  const activeUsers = await database.query(
-    "select * from pg_stat_activity where datname = 'local_db'",
-  );
+  const activeUsers = await database.query({
+    text: `select count(*)::int as total from pg_stat_activity where datname = $1;`,
+    values: [dataName],
+  });
+  console.log(dataName);
   return response.status(200).json({
     update_at: updateAt,
     database: {
       version: version.rows[0].server_version,
       max_connections: parseInt(maxConnections.rows[0].max_connections),
-      active_users: activeUsers.rows.length,
+      active_users: activeUsers.rows[0].total,
     },
   });
 }
