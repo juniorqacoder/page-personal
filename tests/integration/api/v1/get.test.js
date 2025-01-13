dbc82462635272f071996e1c508e42c520697966
Junior | QA & Dev <59706105+juniorqadev@users.noreply.github.com>
2025-01-13 15:57:06 +0000

    Reincluded tests

---
+++ tests/integration/api/v1/get.test.js
@@ -0,0 +1,12 @@
+test("Get to /api/status should return 200 and health check", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(response.status).toBe(200);
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+
+  expect(responseBody.update_at).toBe(parsedUpdateAt);
+  expect(responseBody.database.version).toBe("17.2");
+  expect(responseBody.database.max_connections).toBe(100);
+  expect(responseBody.database.active_users).toBeGreaterThan(0)
+});
