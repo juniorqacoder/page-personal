5319d6cb6c65441fb670b068c1f228c3cf4ede7a
Junior | QA & Dev <59706105+juniorqadev@users.noreply.github.com>
2025-01-22 13:58:14 +0000

    Log erro client

--- infra/database.js
+++ infra/database.js
@@ -5,6 +5,7 @@ async function query(queryObject) {
 
   try {
     client = await getNewClient();
+    console.log(client);
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
