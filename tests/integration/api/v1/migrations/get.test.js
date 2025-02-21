587167a06fc3cb753966d9bf1c8c351e208e2921
Junior | QA & Dev <59706105+juniorqadev@users.noreply.github.com>
2025-02-21 20:06:29 +0000

    test: adjust `tests names`and `orchestrator clear database`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,22 +1,16 @@
-import database from 'infra/database';
 import orchestrator from 'tests/orchestrator.js';
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
-  await cleanDatabase();
+  await orchestrator.clearDatabase();
 });
 
-test('Get to /api/v1/migrations should return 200 and health check', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/migrations');
-  const responseBody = await response.json();
-  expect(response.status).toBe(200);
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+describe('GET to /api/v1/migrations', () => {
+  test('should return pending migrations', async () => {
+    const response = await fetch('http://localhost:3000/api/v1/migrations');
+    const responseBody = await response.json();
+    expect(response.status).toBe(200);
+    expect(Array.isArray(responseBody)).toBe(true);
+    expect(responseBody.length).toBeGreaterThan(0);
+  });
 });
-
-async function cleanDatabase() {
-  await database.query(`
-    drop schema public cascade; 
-    create schema public;
-    `);
-}
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,15 +3,18 @@ import orchestrator from 'tests/orchestrator.js';
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
-test('Get to /api/v1/status should return 200 and health check', async () => {
-  const response = await fetch('http://localhost:3000/api/v1/status');
-  const responseBody = await response.json();
 
-  expect(response.status).toBe(200);
-  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+describe('Get to /api/v1/status', () => {
+  test('should return status database', async () => {
+    const response = await fetch('http://localhost:3000/api/v1/status');
+    const responseBody = await response.json();
 
-  expect(responseBody.update_at).toBe(parsedUpdateAt);
-  expect(responseBody.database.version).toBe('17.2');
-  expect(responseBody.database.max_connections).toBe(100);
-  expect(responseBody.database.active_users).toBeGreaterThan(0);
+    expect(response.status).toBe(200);
+    const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+
+    expect(responseBody.update_at).toBe(parsedUpdateAt);
+    expect(responseBody.database.version).toBe('17.2');
+    expect(responseBody.database.max_connections).toBe(100);
+    expect(responseBody.database.active_users).toBeGreaterThan(0);
+  });
 });
