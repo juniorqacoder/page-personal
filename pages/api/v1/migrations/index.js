baa188393673eb8da7850c5fb427d1b9930c1308
Gilmario Junior <gilmario@qaxsolutions.com>
2025-05-23 22:46:26 -0300

    fix: adjust migration index

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,6 +1,6 @@
-import { createRouter } from "next-connect";
-import controller from "infra/controller.js";
-import migrator from "models/migrator.js";
+import { createRouter } from 'next-connect';
+import controller from 'infra/controller.js';
+import migrator from 'models/migrator.js';
 
 const router = createRouter();
 
