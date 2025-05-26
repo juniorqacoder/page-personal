e150a6c2a5b4b5d75d7a2dd4de1d65df42515115
Gilmario Junior <gilmario@qaxsolutions.com>
2025-05-26 10:15:56 -0300

    feat: update orchestrator.js with runPendingMIgrations

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -1,12 +1,13 @@
 import retry from 'async-retry';
 import database from 'infra/database';
+import migrator from 'models/migrator';
 
 async function waitForAllServices() {
   await waitForWebServer();
   async function waitForWebServer() {
     return retry(fetchStatusPage, {
-      maxTimeout: 1000,
       retries: 100,
+      maxTimeout: 1000,
     });
     async function fetchStatusPage() {
       const response = await fetch('http://localhost:3000/api/v1/status');
@@ -23,9 +24,14 @@ async function clearDatabase() {
     `);
 }
 
+async function runPendingMigrations() {
+  await migrator.runPendingMigrations();
+}
+
 const orchestrator = {
   waitForAllServices,
   clearDatabase,
+  runPendingMigrations,
 };
 
 export default orchestrator;
