9ecbf11686d43c05eb8571de668a282ccd117542
Gilmario Junior <gilmario@qaxsolutions.com>
2025-06-04 10:08:47 -0300

    feat: create api and test for create user

---
+++ tests/integration/api/v1/users/[username]/get.test.js
@@ -0,0 +1,89 @@
+import database from 'infra/database';
+import orchestrator from 'tests/orchestrator.js';
+import { version as uuidVersion } from 'uuid';
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe('GET to /api/v1/users/[username]', () => {
+  describe('Anonymous user', () => {
+    test('With exact case match', async () => {
+      await fetch('http://localhost:3000/api/v1/users', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: 'gilmario',
+          email: 'gilmario@qaxsolutions.com',
+          password: 'senha123',
+        }),
+      });
+
+      const response2 = await fetch(
+        'http://localhost:3000/api/v1/users/gilmario',
+      );
+
+      expect(response2.status).toBe(200);
+
+      const response2Body = await response2.json();
+      expect(response2Body).toEqual({
+        id: response2Body.id,
+        username: 'gilmario',
+        email: 'gilmario@qaxsolutions.com',
+        create_at: response2Body.create_at,
+        update_at: response2Body.update_at,
+        password: 'senha123',
+      });
+      expect(uuidVersion(response2Body.id)).toBe(4);
+      expect(Date.parse(response2Body.create_at)).not.toBeNaN();
+      expect(Date.parse(response2Body.update_at)).not.toBeNaN();
+    });
+    test('With case mismatch', async () => {
+      await fetch('http://localhost:3000/api/v1/users', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          username: 'MisMatch',
+          email: 'mismatch@qaxsolutions.com',
+          password: 'senha123',
+        }),
+      });
+
+      const response2 = await fetch(
+        'http://localhost:3000/api/v1/users/mismatch',
+      );
+
+      expect(response2.status).toBe(200);
+
+      const response2Body = await response2.json();
+      expect(response2Body).toEqual({
+        id: response2Body.id,
+        username: 'MisMatch',
+        email: 'mismatch@qaxsolutions.com',
+        create_at: response2Body.create_at,
+        update_at: response2Body.update_at,
+        password: 'senha123',
+      });
+      expect(uuidVersion(response2Body.id)).toBe(4);
+      expect(Date.parse(response2Body.create_at)).not.toBeNaN();
+      expect(Date.parse(response2Body.update_at)).not.toBeNaN();
+    });
+    test('With nonexistent user', async () => {
+      const response = await fetch(
+        'http://localhost:3000/api/v1/users/nonexistent',
+      );
+
+      expect(response.status).toBe(404);
+
+      const responseBody = await response.json();
+      expect(responseBody).toEqual({
+        name: 'NotFoundError',
+        message: 'Não encontraro nenhum username',
+        action: 'Verificar se o username está correto',
+        status_code: 404,
+      });
+    });
+  });
+});
