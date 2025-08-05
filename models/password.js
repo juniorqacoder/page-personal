b936fc2e0624ad83c9ff2eae7c86e78527ea279c
Gilmario Junior <gilmario@qaxsolutions.com>
2025-08-05 10:21:16 -0300

    fix: adjust rounds bcrypto

--- models/password.js
+++ models/password.js
@@ -6,7 +6,7 @@ async function hash(password) {
 }
 
 async function getNumbersOfRounds() {
-  return process.env.NODE_ENV === 'production' ? 14 : 1;
+  return process.env.NODE_ENV === 'production' ? 14 : 4;
 }
 
 async function compare(providedPassword, storedPassword) {
